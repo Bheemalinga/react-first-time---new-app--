9444db357739012c3e9dad871b1243b2624fc45f
Bheemalinga <kummarabheema@gmail.com>
2023-08-28 12:32:02 +0530

    Modification of App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contacts from './pages/contacts'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Portfolio from './pages/Portfolio'
+import Website from './pages/Website'
+import Technology from './pages/Technology'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/contacts" element={<Contacts/>} />
+          <Route path="/portfolio" element={<Portfolio/>} />
+          <Route path="/website" element={<Website/>} />
+          <Route path="/technology" element={<Technology/>} />
+
+        </Routes>
+        <Footer/>
+
+
+    </Router>
+  )
 }
 
-export default App;
+export default App
